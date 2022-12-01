83f657c73d57aaab1c591c2bbc8aa772eca605c6
jacobjwood <jacobjwood@gmail.com>
2022-12-01 13:50:00 +0000

    Added gitignore to staging

--- day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/Users/jawood/aoc2022/day1/target/debug/day1: /Users/jawood/aoc2022/day1/src/main.rs
--- day1/target/debug/deps/day1-d054eb58c4f26c14.d
+++
@@ -1,5 +0,0 @@
-/Users/jawood/aoc2022/day1/target/debug/deps/day1-d054eb58c4f26c14: src/main.rs
-
-/Users/jawood/aoc2022/day1/target/debug/deps/day1-d054eb58c4f26c14.d: src/main.rs
-
-src/main.rs:
