6b08a23b831f38e8b1f4b397d9c6fb246d30de8c
jacobjwood <jacobjwood@gmail.com>
2022-12-17 01:46:16 +0000

    I can smell that I am close

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -69,7 +69,8 @@ fn main() {
     get_best_valve(&valve_flow_rates, &30, &"AA", &valve_distances);
 
     // DD BB JJ HH EE CC
-    traversal(&valve_flow_rates, &valve_distances, &"AA", 30);
+    //traversal(&valve_flow_rates, &valve_distances, &"AA", 30);
+    traversal_w_e(&valve_flow_rates, &valve_distances, &"AA", 26);
 }
 
 fn traversal(vfr: &HashMap<&str, usize>, vd: &HashMap<&str, HashMap<&str, usize>>, start_state: &str, max_time: usize) {
@@ -80,8 +81,6 @@ fn traversal(vfr: &HashMap<&str, usize>, vd: &HashMap<&str, HashMap<&str, usize>
 
     while let Some((mut traj, mut ct, mut tfr, mut cf)) = traj_vec.pop() {
         //println!("{}", ct);
-
-        //println!("{:?}", traj);
 
         if false {
             println!("CURRENT TIME: {}, TOTAL FLOW RATE: {}, CUMULATIVE FLOW: {}", ct, tfr, cf);
@@ -155,12 +154,332 @@ fn traversal(vfr: &HashMap<&str, usize>, vd: &HashMap<&str, HashMap<&str, usize>
     println!("{}", scores.len());
 }
 
-fn traversal2(vfr: &HashMap<&str, usize>, vd: &HashMap<&str, HashMap<&str, usize>>, max_time: usize) {
+#[derive(Clone)]
+struct MeOrE<'a> {
+    trajectory: Vec<&'a str>,
+    round_opened: Vec<usize>,
+    flow_rate_after_round: Vec<usize>,
+    time: usize,
+    flow_rate: usize,
+    total_flow: usize,
+}
 
-    // Have a HashMap of distances
-    // start from initial state
+fn traversal_w_e(vfr: &HashMap<&str, usize>, vd: &HashMap<&str, HashMap<&str, usize>>, start_state: &str, max_time: usize) {
+    // me, elephant, cumulative flow
+    let mut traj_vec : Vec<(MeOrE, MeOrE)> = Vec::new();
+
+    let mut me = MeOrE{ trajectory : vec![&"AA"],
+                        round_opened: vec![0],
+                        flow_rate_after_round: Vec::new(),
+                        time : 0 ,
+                        flow_rate : 0,
+                        total_flow : 0,
+                    };
+
+    let mut elephant = MeOrE{ trajectory : vec![&"AA"],
+                        round_opened: vec![0],
+                        flow_rate_after_round: Vec::new(),
+                        time : 0 ,
+                        flow_rate : 0,
+                        total_flow : 0,
+                };
+
+    
+    traj_vec.push((me, elephant));
+    let mut scores : Vec<usize> = Vec::new();
+
+    
+    while let Some((mut me, mut elephant)) = traj_vec.pop() {
+
+        println!("{:?}", traj_vec.len());
+
+        // first off generate the set of possible states left to explore for the free agents
+        let traj_me_set : HashSet<&str> = HashSet::from_iter(me.trajectory.iter().cloned());
+        let traj_e_set : HashSet<&str> = HashSet::from_iter(elephant.trajectory.iter().cloned());
+        let traj_set : HashSet<&str> = traj_me_set.union(&traj_e_set).map(|s| *s).collect();
+        
+        // get our current states (although we may not be free yet)
+        let my_state = me.trajectory.last().unwrap();
+        let e_state = elephant.trajectory.last().unwrap();
+
+        // get the current time. this is the minimum of the times we are in
+        let ct = std::cmp::min(me.time, elephant.time);
+
+        // println!("Current time {}", ct);
+
+        // if its my turn to choose only, then this becomes like part 1
+        if ct == me.time && ct != elephant.time {
+            // println!("It's my time");
+
+            let state = me.trajectory.last().unwrap();
+
+            let mut poss_next : Vec<(&str, usize)> = vd
+                .get(state)
+                .unwrap()
+                .to_owned()
+                .into_iter()
+                .filter(|(k, v)| !traj_set.contains(*k) && *vfr.get(k).unwrap() != 0 && (v + 1) < (max_time - ct))
+                .collect();
+
+            // if out of options, then we need to coerce the times
+            if poss_next.len() == 0 {
+                let mut new_me = me.to_owned();
+                new_me.total_flow += new_me.flow_rate * (elephant.time - new_me.time);
+                new_me.time = elephant.time;
+                traj_vec.push((new_me, elephant.to_owned()));
+            }
+            
+            // iterate over all possible next states
+            for (k, v) in &poss_next {
+                // println!("{:?} {:?}", k, v);
+                let flow_rate = vfr.get(k).unwrap();
+                let distance_to_and_turn_on = v + 1;
+                let time_after_turning_on = distance_to_and_turn_on + ct;
+    
+                if time_after_turning_on <= max_time {
+    
+                    //println!("{} {} {} {}", k, ct, distance_to_and_turn_on, flow_rate);
+                    let mut new_traj = me.trajectory.to_owned();
+                    let mut new_ro = me.round_opened.to_owned();
+                    new_ro.push(time_after_turning_on);
+                    new_traj.push(k);
+                    let mut frar = me.flow_rate_after_round.to_owned();
+                    frar.push(me.flow_rate + flow_rate);
+                    let new_me = MeOrE { trajectory : new_traj, round_opened : new_ro, flow_rate_after_round : frar, time : time_after_turning_on, flow_rate : me.flow_rate + flow_rate, total_flow : me.flow_rate * distance_to_and_turn_on };
+                    //println!("PUSHING");
+                    //println!("{}", flow_rate);
+                    let to_push = (new_me, elephant.to_owned());
+                    // println!("{:?}", to_push);
+                    traj_vec.push(to_push);
+                }
+            }
+
+        } else if ct == elephant.time && ct != me.time {
+            // println!("It's elephant time");
+
+            let state = elephant.trajectory.last().unwrap();
+
+            let mut poss_next : Vec<(&str, usize)> = vd
+                .get(state)
+                .unwrap()
+                .to_owned()
+                .into_iter()
+                .filter(|(k, v)| !traj_set.contains(*k) && *vfr.get(k).unwrap() != 0 && (v + 1) < (max_time - ct))
+                .collect();
+
+            if poss_next.len() == 0 {
+                let mut new_elephant = elephant.to_owned();
+                new_elephant.total_flow += new_elephant.flow_rate * (me.time - new_elephant.time);
+                new_elephant.time = me.time;
+                traj_vec.push((me.to_owned(), new_elephant));
+            }
+            
+            // iterate over all possible next states
+            for (k, v) in &poss_next {
+                // println!("{:?} {:?}", k, v);
+                let flow_rate = vfr.get(k).unwrap();
+                let distance_to_and_turn_on = v + 1;
+                let time_after_turning_on = distance_to_and_turn_on + ct;
+    
+                if time_after_turning_on <= max_time {
+    
+                    //println!("{} {} {} {}", k, ct, distance_to_and_turn_on, flow_rate);
+                    let mut new_traj = elephant.trajectory.to_owned();
+                    let mut new_ro = elephant.round_opened.to_owned();
+                    new_traj.push(k);
+                    new_ro.push(time_after_turning_on);
+                    let mut frar = elephant.flow_rate_after_round.to_owned();
+                    frar.push(elephant.flow_rate + flow_rate);
+                    let new_elephant = MeOrE { trajectory : new_traj, round_opened : new_ro, flow_rate_after_round : frar, time : time_after_turning_on, flow_rate : elephant.flow_rate + flow_rate, total_flow : elephant.flow_rate * distance_to_and_turn_on };
+                    //println!("PUSHING");
+                    //println!("{}", flow_rate);
+                    let to_push = (me.to_owned(), new_elephant);
+                    // println!("{:?}", to_push);
+                    traj_vec.push(to_push);
+                }
+            }    
+            
+        } else {
+            // Both our times
+            // println!("Both our times");
+            let me_state = me.trajectory.last().unwrap();
+            let e_state = elephant.trajectory.last().unwrap();
+
+            let mut poss_next_me : Vec<(&str, usize)> = vd
+                .get(me_state)
+                .unwrap()
+                .to_owned()
+                .into_iter()
+                .filter(|(k, v)| !traj_set.contains(*k) && *vfr.get(k).unwrap() != 0 && (v + 1) < (max_time - ct))
+                .collect();
+
+            let mut poss_next_e : Vec<(&str, usize)> = vd
+                .get(e_state)
+                .unwrap()
+                .to_owned()
+                .into_iter()
+                .filter(|(k, v)| !traj_set.contains(*k) && *vfr.get(k).unwrap() != 0 && (v + 1) < (max_time - ct))
+                .collect();
+
+            // if space is exhausted, just tally up flow rates
+            if poss_next_me.is_empty() && poss_next_e.is_empty() {
 
 
+                // This is where I get it
+                me.flow_rate_after_round.push(0);
+                me.round_opened.push(max_time);
+                elephant.round_opened.push(max_time);
+                me.flow_rate_after_round.sort();
+                elephant.flow_rate_after_round.push(0);
+                elephant.flow_rate_after_round.sort();
+
+                let mut score1 = 0;
+
+                for idx in 0..me.round_opened.len()-1 {
+                    score1 += (me.round_opened[idx+1] - me.round_opened[idx]) * me.flow_rate_after_round[idx];
+                }
+
+                for idx in 0..elephant.round_opened.len()-1 {
+                    score1 += (elephant.round_opened[idx+1] - elephant.round_opened[idx]) * elephant.flow_rate_after_round[idx];
+                }
+
+
+
+
+
+
+                me.total_flow += (max_time - ct) * me.flow_rate;
+                elephant.total_flow += (max_time - ct) * elephant.flow_rate;
+                // JJ BB CC = ME
+                // DD HH EE = ELEPHANT
+                if false && me.trajectory == vec!["AA", "JJ", "BB", "CC"] && elephant.trajectory == vec!["AA", "DD", "HH", "EE"] {
+                    println!("CT {}", ct);
+                    println!("ME_TRAJ {:?}", me.trajectory);
+                    println!("ME RO {:?}", me.round_opened);
+                    println!("E_TRAJ {:?}", elephant.trajectory);
+                    println!("E RO {:?}", elephant.round_opened);
+                    println!("ME FRAR {:?}", me.flow_rate_after_round);
+                    println!("E FRAR {:?}", elephant.flow_rate_after_round);
+                    println!("EMITTING {:?}", elephant.flow_rate + me.flow_rate);
+                    println!("TOTAL FLOW {:?}", elephant.total_flow + me.total_flow);
+                    std::thread::sleep_ms(4000);
+                }
+                if false && me.total_flow + elephant.total_flow == 1612 {
+                    println!("CT {}", ct);
+                    println!("ME_TRAJ {:?}", me.trajectory);
+                    println!("ME RO {:?}", me.round_opened);
+                    println!("E_TRAJ {:?}", elephant.trajectory);
+                    println!("E RO {:?}", elephant.round_opened);
+                    println!("ME FRAR {:?}", me.flow_rate_after_round);
+                    println!("E FRAR {:?}", elephant.flow_rate_after_round);
+                    println!("EMITTING {:?}", elephant.flow_rate + me.flow_rate);
+                    println!("TOTAL FLOW {:?}", elephant.total_flow + me.total_flow);
+                    std::thread::sleep_ms(4000);
+                }
+                //scores.push(me.total_flow + elephant.total_flow);
+                scores.push(score1);
+           } else if poss_next_me.is_empty() && !poss_next_e.is_empty() {
+                me.total_flow += (max_time - ct) * me.flow_rate;
+                me.time = max_time;
+                traj_vec.push((me.to_owned(), elephant.to_owned()));
+            } else if !poss_next_me.is_empty() && poss_next_e.is_empty() {
+                elephant.total_flow += (max_time - ct) * elephant.flow_rate;
+                elephant.time = max_time;
+                traj_vec.push((me.to_owned(), elephant.to_owned()));
+            } else {
+                let state = e_state;
+
+                let mut poss_next = poss_next_e;
+    
+                if poss_next.len() == 0 {
+                    let mut new_elephant = elephant.to_owned();
+                    new_elephant.total_flow += new_elephant.flow_rate * (me.time - new_elephant.time);
+                    new_elephant.time = me.time;
+                    traj_vec.push((me.to_owned(), new_elephant));
+                }
+                
+                // iterate over all possible next states
+                for (k, v) in &poss_next {
+                    // println!("{:?} {:?}", k, v);
+                    let flow_rate = vfr.get(k).unwrap();
+                    let distance_to_and_turn_on = v + 1;
+                    let time_after_turning_on = distance_to_and_turn_on + ct;
+        
+                    if time_after_turning_on <= max_time {
+        
+                        //println!("{} {} {} {}", k, ct, distance_to_and_turn_on, flow_rate);
+                        let mut new_traj = elephant.trajectory.to_owned();
+                        let mut new_ro = elephant.round_opened.to_owned();
+                        new_traj.push(k);
+                        new_ro.push(time_after_turning_on);
+                        let mut frar = elephant.flow_rate_after_round.to_owned();
+                        frar.push(elephant.flow_rate + flow_rate);
+                        let new_elephant = MeOrE { trajectory : new_traj, round_opened : new_ro, flow_rate_after_round : frar, time : time_after_turning_on, flow_rate : elephant.flow_rate + flow_rate, total_flow : elephant.flow_rate * distance_to_and_turn_on };
+                        //println!("PUSHING");
+                        //println!("{}", flow_rate);
+                        let to_push = (me.to_owned(), new_elephant);
+                        // println!("{:?}", to_push);
+                        traj_vec.push(to_push);
+                    }
+                }
+
+                let state = me.trajectory.last().unwrap();
+
+                let mut poss_next : Vec<(&str, usize)> = vd
+                    .get(state)
+                    .unwrap()
+                    .to_owned()
+                    .into_iter()
+                    .filter(|(k, v)| !traj_set.contains(*k) && *vfr.get(k).unwrap() != 0 && (v + 1) < (max_time - ct))
+                    .collect();
+
+                // if out of options, then we need to coerce the times
+                if poss_next.len() == 0 {
+                    let mut new_me = me.to_owned();
+                    new_me.total_flow += new_me.flow_rate * (elephant.time - new_me.time);
+                    new_me.time = elephant.time;
+                    traj_vec.push((new_me, elephant.to_owned()));
+                }
+                
+                // iterate over all possible next states
+                for (k, v) in &poss_next {
+                    // println!("{:?} {:?}", k, v);
+                    let flow_rate = vfr.get(k).unwrap();
+                    let distance_to_and_turn_on = v + 1;
+                    let time_after_turning_on = distance_to_and_turn_on + ct;
+        
+                    if time_after_turning_on <= max_time {
+        
+                        //println!("{} {} {} {}", k, ct, distance_to_and_turn_on, flow_rate);
+                        let mut new_traj = me.trajectory.to_owned();
+                        let mut new_ro = me.round_opened.to_owned();
+                        new_traj.push(k);
+                        new_ro.push(time_after_turning_on);
+                        let mut frar = me.flow_rate_after_round.to_owned();
+                        frar.push(me.flow_rate + flow_rate);
+                        let new_me = MeOrE { trajectory : new_traj, round_opened : new_ro, flow_rate_after_round : frar, time : time_after_turning_on, flow_rate : me.flow_rate + flow_rate, total_flow : me.flow_rate * distance_to_and_turn_on };
+                        //println!("PUSHING");
+                        //println!("{}", flow_rate);
+                        let to_push = (new_me, elephant.to_owned());
+                        // println!("{:?}", to_push);
+                        traj_vec.push(to_push);
+                    }
+                }
+
+
+            }
+
+            
+        }
+
+        
+    }
+
+    scores.sort_by(|a, b| b.cmp(&a));
+    //let scores_print = scores[0..10].to_owned();
+    println!("MAX SCORE {:?}", scores.iter().max());
+    println!("{}", scores.len());
+    
 }
 
 fn get_best_valve(
