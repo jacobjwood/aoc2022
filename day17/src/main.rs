ed5cf4a5b7c4f218f67ea492f9bdaf86ba139953
jacobjwood <jacobjwood@gmail.com>
2022-12-19 11:06:20 +0000

    Scrappy Day 17 part 2, just needed to find some k deep enough where I don't have to worry about the floor being infinite. Will clean up.

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -19,7 +19,7 @@ fn main() {
     // initialise the floor
     let mut settled_set = HashSet::from([(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6)]);
 
-    while rock_count < 2022 {
+    while rock_count < 22 {
 
         // initialise the rock with top left coordinate at 0, 0
         let mut rock = rock_types(&rock_count);
@@ -56,8 +56,16 @@ fn main() {
 
     let mut rc_break = 0;
     let mut hc_break = 0;
+    let mut found_match = false;
+    let mut first_rc_break = 0;
+    let mut first_hc_break = 0;
+    let mut first_cc_break = 0;
+    let mut first_repeat : Vec<(i32, i32)> = Vec::new();
+    let mut target = 10000;
+    let mut first_track = 0;
+    let mut remainder = 0;
 
-    'outer: while rock_count < 2022 {
+    'outer: while rock_count < target {
 
         // initialise the rock with top left coordinate at 0, 0
         let mut rock = rock_types(&rock_count);
@@ -79,7 +87,7 @@ fn main() {
         let depth = settled_set.iter().map(|r| r.0).max().unwrap();
         let min_depth = settled_set.iter().map(|r| r.0).min().unwrap();
 
-        println!("DEPTH WHAT {}", depth - min_depth);
+        //println!("DEPTH WHAT {}", depth - min_depth);
 
         let mut breadth_set : HashSet<i32> = HashSet::new();
 
@@ -90,45 +98,83 @@ fn main() {
             breadth_set.extend(bs);
 
             if breadth_set.len() == 7 {
-                println!("New set found {}", k);
+                //println!("New set found {}", k);
 
                 //println!("JEFF {} {} {}", min_depth, depth, k);
-                println!("D-MD {}", depth - min_depth);
-                settled_set = settled_set.into_iter().filter(|r| r.0 <= k + 5).collect();
+                // println!("D-MD {}", depth - min_depth);
+                settled_set = settled_set.into_iter().filter(|r| r.0 <= k + 200).collect();
                 let new_depth = settled_set.iter().map(|r| r.0).max().unwrap();
 
                 if new_depth - min_depth == depth - min_depth {
                     // do nothing
                 } else {
                     height_count += (depth - min_depth) - (new_depth - min_depth);
-                    println!("height_count {}", height_count);
+                    // println!("height_count {}", height_count);
                 }
 
-                println!("{}", k == depth);
-                println!("{}", new_depth - min_depth);
+                //println!("{}", k == depth);
+                //println!("{}", new_depth - min_depth);
 
-                println!("DEPTH_NEW {}", new_depth);
-                println!("{} {}", k + 5, min_depth);
+                //println!("DEPTH_NEW {}", new_depth);
+                //println!("{} {}", k + 5, min_depth);
                 let mut ss = settled_set.iter().cloned().collect::<Vec<(i32, i32)>>();
                 ss.sort();
 
                 if floor_set[rock_count % 5].contains(&ss) {
+
+                    if found_match {
+                        if ss == first_repeat && rock_count % 5 == first_rc_break % 5 {
+                            println!("HELLO IT'S REPEATED");
+                            settled_set = HashSet::from_iter(ss.to_owned().into_iter());
+                            print_grid(&settled_set);
+                            let rc_diff = rock_count - first_rc_break;
+                            let hc_diff = height_count + settled_set.iter().map(|r| r.0).max().unwrap() - settled_set.iter().map(|r| r.0).min().unwrap() - first_hc_break;
+                            let cc_diff = cycle_count - first_cc_break;
+
+                            let n_repetitions = (1000000000000 - (rock_count) as i64) as i64 / rc_diff as i64;
+                            println!("UH {} {} {} {}", rc_diff, hc_diff, first_hc_break, first_cc_break);
+                            // height with remaining iters
+                            println!("remainder {}", (1000000000000 - (rock_count + rc_diff) as i64) as i64 % rc_diff as i64);
+                            println!("{} {}", (rock_count as i64 + ((n_repetitions + 1) * rc_diff as i64)) % 5, (cycle_count as i64 + ((n_repetitions + 1) * cc_diff as i64) as i64) % blow_stream.len() as i64);
+                            first_track = first_hc_break as i64 + (hc_diff as i64 * (n_repetitions + 1));
+                            remainder = ((1000000000000 - (rock_count + rc_diff) as i64) as i64 % rc_diff as i64) as usize;
+                            rock_count = ((rock_count as i64 + ((n_repetitions + 1) * rc_diff as i64)) % 5) as usize;
+                            cycle_count = ((cycle_count as i64 + ((n_repetitions + 1) * cc_diff as i64) as i64) % blow_stream.len() as i64) as usize; 
+                            println!("{}", first_hc_break as i64 + (hc_diff as i64 * (n_repetitions + 1)));
+                            // 82 out
+                            break 'outer;
+                        }
+                    }
+
+                    // If first time finding match, keep track
+                    if !found_match {
+                        first_rc_break = rock_count;
+                        first_hc_break = height_count + settled_set.iter().map(|r| r.0).max().unwrap() - settled_set.iter().map(|r| r.0).min().unwrap();
+                        first_cc_break = cycle_count;
+                        first_repeat = ss.to_owned();
+                        settled_set = HashSet::from_iter(ss.to_owned().into_iter());
+                        print_grid(&settled_set);
+                        found_match = true;
+                    }
+
+                    
+
+                    /*
                     println!("RC: {}", rock_count);
                     rc_break = rock_count;
-                    hc_break = height_count + settled_set.iter().map(|r| r.0).max().unwrap() - settled_set.iter().map(|r| r.0).min().unwrap();
+                    hc_break = 
                     println!("rc_break {} hc_break {}", rc_break, hc_break);
-                    let begin_count = true;
                     settled_set = HashSet::from_iter(ss.into_iter());
                     break 'outer;
-                } else {
+                    */ 
+                } else if !found_match {
                     floor_set[rock_count % 5].insert(ss);
                 }   
                 //print_grid(&settled_set);
                 break;
             }
         }
-
-        // RELOOP
+        
 
 
 
@@ -141,7 +187,42 @@ fn main() {
         //print_grid(&settled_set);
     };
 
-    println!("{} {}", rc_break, hc_break);
+    // initialise the floor
+    //let mut settled_set = HashSet::from([(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6)]);
+
+    print_grid(&settled_set);
+    let ss_1 = settled_set.iter().map(|r| r.0).max().unwrap() - settled_set.iter().map(|r| r.0).min().unwrap();
+    let rmder = remainder as usize + rock_count as usize;
+
+    while rock_count < rmder.try_into().unwrap() {
+
+        //println!("{}", ss_1);
+
+        // initialise the rock with top left coordinate at 0, 0
+        let mut rock = rock_types(&rock_count);
+        // left starts 2 away from the wall
+        rock = rock.iter().map(|r| (r.0, r.1 + 2)).collect();
+        // get max depth of rock segment and push everything settled "further down" so that it is 3 away
+        let rock_height = rock.iter().map(|r| r.0).max().unwrap();
+        let settled_height = settled_set.iter().map(|r| r.0).min().unwrap();
+        settled_set = settled_set.iter().map(|r| (r.0 + rock_height + 4 - settled_height, r.1)).collect();
+
+        // check collision
+        blow_rock(rock, &mut settled_set, &blow_stream, &mut cycle_count);
+
+        
+        //println!("LEN SS {}", settled_set.len());
+        rock_count += 1;
+
+       
+    }
+
+    //print_grid(&settled_set);
+
+    println!("Part 1: {:?}", first_track + settled_set.iter().map(|r| r.0).max().unwrap() as i64 - settled_set.iter().map(|r| r.0).min().unwrap() as i64 - ss_1 as i64);
+
+
+    //println!("{} {}", rc_break, hc_break);
     println!("HC: {}", height_count + settled_set.iter().map(|r| r.0).max().unwrap() - settled_set.iter().map(|r| r.0).min().unwrap());
 
     // 63, 102 periodicity so 
